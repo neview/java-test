479eec3321caf2093c51fe96ec90dfc241d4eced
芒果集团 <1282261308@qq.com>
2024-09-06 17:56:00 +0800

    '数据类型'

---
+++ sports-event/src/Main.java
@@ -0,0 +1,75 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+//        System.out.printf("Hello and welcome!");
+//
+//        for (int i = 1; i <= 5; i++) {
+//            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+//            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+//            System.out.println("i = " + i);
+//        }
+
+//        int a = 100;
+//        System.out.println(a);
+//        a = 200;
+//        System.out.println("第二次" + a);
+
+//        int n = 100; // 定义变量n，同时赋值为100
+//        System.out.println("n = " + n); // 打印n的值
+//
+//        n = 200; // 变量n赋值为200
+//        System.out.println("n = " + n); // 打印n的值
+//
+//        int x = n; // 变量x赋值为n（n的值为200，因此赋值后x的值也是200）
+//        System.out.println("x = " + x); // 打印x的值
+//
+//        x = x + 100; // 变量x赋值为x+100（x的值为200，因此赋值后x的值是200+100=300）
+//        System.out.println("x = " + x); // 打印x的值
+//        System.out.println("n = " + n); // 再次打印n的值，n应该是200还是300？
+
+//          int a = 100 / 0;
+//          System.out.println(a);
+
+//         int a = 100;
+//         long b = 100 + (++a);
+//         System.out.println(b);
+
+//            int a = 100;
+//            int b = a << 1;
+//            System.out.println(b);
+
+//            double a = 1.0 * 10;
+//            double b = 1 - 9.0 / 10;
+//            System.out.println(a);
+//            System.out.println(b);
+
+            double a = Math.abs(1.0-1.00);
+            if(a < 0.00001){
+                System.out.println("相等");
+            } else {
+                System.out.println("不相等");
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
